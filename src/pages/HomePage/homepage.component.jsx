f0be94940de2895095d9da00a8a28a1aa821599f
Avinash Kumar <avinash.kumar@symphonyretailai.com>
2021-04-25 16:03:57 +0530

    basic component structure and css

---
+++ src/pages/HomePage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from'react';
+import Directory from '../../Components/directory/directory.component';
+import './homepage.scss'
+export const HomePage=()=>(
+   
+        <div className="homepage">
+        
+        <Directory/>     
+   </div>
+    
+)
